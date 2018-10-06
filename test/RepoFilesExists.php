31ba29157c48a70a3fa625f2edb419b9c1f04996
Jessica Mauerhan <jessicamauerhan@gmail.com>
2018-10-06 13:23:47 -0500

    Re-enable test

--- test/RepoFilesExists.php
+++ test/RepoFilesExists.php
@@ -7,8 +7,8 @@ class RepoFilesExistsTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
     	$this->requiredFiles = [
-            ['./Dockerfile', './docker/Dockerfile'],
-            ['./docker-compose.yml', './docker/docker-compose.yml'],
+//            ['./Dockerfile', './docker/Dockerfile'],
+//            ['./docker-compose.yml', './docker/docker-compose.yml'],
             './.codeclimate.yml',
             './.env_sample',
             './.github/ISSUE_TEMPLATE',
@@ -28,7 +28,6 @@ class RepoFilesExistsTest extends \PHPUnit_Framework_TestCase
 
     public function testFileArePresentInRepo()
     {
-		$this->markTestSkipped('Temporarily skip test');
         foreach ($this->requiredFiles as $filePath) {
             if (is_array($filePath)) {
                 $exists = array_filter(
